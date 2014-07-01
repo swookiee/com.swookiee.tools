1a63d22c7ce85b9092419a138c4d43ffb202cea0
Lars Pfannenschmidt <lars@mobilecologne.de>
2014-07-01 15:22:47 +0200

    Enabled deployment of maven dependencies defined in a pom.

--- com.swookiee.tools.deployplugin/src/main/java/com/swookiee/tools/com/swookiee/tools/deployplugin/SwookieeInstall.java
+++ com.swookiee.tools.deployplugin/src/main/java/com/swookiee/tools/com/swookiee/tools/deployplugin/SwookieeInstall.java
@@ -1,7 +1,14 @@
 package com.swookiee.tools.com.swookiee.tools.deployplugin;
 
 import java.io.File;
+import java.util.List;
+import java.util.Set;
 
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.resolver.ArtifactNotFoundException;
+import org.apache.maven.artifact.resolver.ArtifactResolutionException;
+import org.apache.maven.artifact.resolver.ArtifactResolver;
+import org.apache.maven.model.Dependency;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 
@@ -55,6 +62,13 @@ public class SwookieeInstall extends AbstractMojo {
     private boolean useHttps;
 
     /**
+     * Do you want to deploy dependencies as well?
+     * 
+     * @parameter default-value="false"
+     */
+    private boolean deployDependencies;
+
+    /**
      * The name of the generated JAR file.
      * 
      * @parameter default-value="${project.build.directory}/${project.build.finalName}.jar"
@@ -62,13 +76,80 @@ public class SwookieeInstall extends AbstractMojo {
      */
     private String bundleFile;
 
+    /**
+     * The set of dependencies required by the project
+     * 
+     * @parameter default-value="${project.dependencyArtifacts}"
+     * @required
+     * @readonly
+     */
+    private Set<Artifact> artifacts;
+
+    /**
+     * The set of dependencies required by the project
+     * 
+     * @parameter default-value="${project.dependencies}
+     * @required
+     * @readonly
+     */
+    private List<Dependency> dependencies;
+
+    /** @component */
+    private ArtifactResolver resolver;
+
+    /** @parameter default-value="${localRepository}" */
+    private org.apache.maven.artifact.repository.ArtifactRepository localRepository;
+
+    /** @parameter default-value="${project.remoteArtifactRepositories}" */
+    @SuppressWarnings("rawtypes")
+    private List remoteRepositories;
+
     @Override
     public void execute() throws MojoExecutionException {
         try (SwookieeClient swookieeClient = getSwookieeClient()) {
-            forceInstallAndStartBundle(swookieeClient, new File(this.bundleFile));
+            deployFile(swookieeClient, this.bundleFile);
+            if (deployDependencies) {
+                deployDependencies(swookieeClient);
+            }
         } catch (final SwookieeClientException ex) {
             getLog().error("Could not deploy bundle: " + ex.getMessage(), ex);
         }
+    }
+
+    private void deployFile(SwookieeClient swookieeClient, String filePath) throws SwookieeClientException {
+
+        if (filePath == null) {
+            getLog().info("No Path to file given");
+            return;
+        }
+
+        File bundle = new File(filePath);
+        if (bundle != null && bundle.exists()) {
+            forceInstallAndStartBundle(swookieeClient, bundle);
+        } else {
+            getLog().warn("Bundle '" + this.bundleFile + "' could not been found!");
+        }
+    }
+
+    private void deployDependencies(SwookieeClient swookieeClient) throws SwookieeClientException {
+        for (Dependency dependency : dependencies) {
+            for (Artifact artifact : artifacts) {
+                if (equalsDependencyArtefact(dependency, artifact)) {
+                    try {
+                        resolver.resolve(artifact, remoteRepositories, localRepository);
+                        deployFile(swookieeClient, artifact.getFile().getPath());
+                    } catch (ArtifactResolutionException | ArtifactNotFoundException e) {
+                        getLog().error("Could not resolve dependency: " + dependency.getArtifactId(), e);
+                    }
+                }
+            }
+        }
+    }
+
+    private boolean equalsDependencyArtefact(Dependency dependency, Artifact artifact) {
+        return artifact.getGroupId().equals(dependency.getGroupId())
+                && artifact.getArtifactId().equals(dependency.getArtifactId())
+                && artifact.getVersion().equals(dependency.getVersion());
     }
 
     private SwookieeClient getSwookieeClient() throws SwookieeClientException {
@@ -86,6 +167,11 @@ public class SwookieeInstall extends AbstractMojo {
         getLog().info(
                 String.format("Installing %s to %s", file.getAbsolutePath(), swookieeClient.getConfiguredTarget()));
         final String installedBundle = swookieeClient.installBundle(file, true);
-        swookieeClient.startBundle(installedBundle);
+        try{
+            swookieeClient.startBundle(installedBundle);
+        } catch (Exception ex) {
+            getLog().warn("Could not start Bundle: " + file.toString());
+        }
+
     }
 }
